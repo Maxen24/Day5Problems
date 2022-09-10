2c9bab4cf154ca81b310681d8e0caa39ae698ead
Maxen24 <mshubham242@gmail.com>
2022-09-10 08:26:09 +0530

    Added Quotient and Remainder Problems

---
+++ src/com/bridgelabz/quotientremainder.java
@@ -0,0 +1,25 @@
+/**
+ * 
+ */
+package com.bridgelabz;
+
+/**
+ * @author mshub
+ *
+ */
+public class quotientremainder {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x=40;
+		int y=20;
+		int quotient=x/y;
+		System.out.println("Quotient is"+" "+quotient);
+		int remainder= x%y;
+		System.out.println("Remainder is"+" "+remainder);
+
+	}
+
+}
